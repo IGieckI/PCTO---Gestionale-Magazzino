f8019b9fb87f2071c79a799b36656824b42e24a4
Federico <federicoioschy@gmail.com>
2021-02-02 10:18:43 +0100

    asdfsff

--- TPSIT-PCTO-Server/MainWindow.xaml.cs
+++ TPSIT-PCTO-Server/MainWindow.xaml.cs
@@ -12,6 +12,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
 
 namespace TPSIT_PCTO_Server
 {
@@ -20,9 +23,56 @@ namespace TPSIT_PCTO_Server
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Thread> accept;
+        Thread connectionRequest;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                accept = new List<Thread>();
+                IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+                IPAddress iPAddress = ipHostInfo.AddressList[1];
+                //IPAddress iPAddress = IPAddress.Parse("10.12.0.28");
+                IPEndPoint localEndPoint = new IPEndPoint(iPAddress, 11000); //creo un endpoint con il mio ip e la porta di comunicazione
+                accept = new List<Thread>();
+                Socket listener = new Socket(iPAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp); //crea la socket
+                connectionRequest = new Thread(() => Connect(listener));
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+        private void Connect(Socket listener)
+        {
+            while(true)
+            {
+                Socket handler = listener.Accept();
+                accept.Add(new Thread(() => AcceptClient(handler)));
+                accept[accept.Count - 1].Start();
+            }
+        }
+        private void AcceptClient(Socket handler)
+        {
+            while(true)
+            {
+                byte[] bytes = new byte[1024]; //buffer dei dati
+                string data = null; //messaggio
+                bool ok = false; //bool per uscire
+                string mex = "";
+                while(!ok)
+                {
+                    mex = "";
+                    int bytesRec = handler.Receive(bytes); //riceve i bytes
+                    data = "";
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec); //trasforma in stringa
+                }
+            }
         }
     }
 }
