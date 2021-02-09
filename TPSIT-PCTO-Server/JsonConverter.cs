391c9f4d81097ee108db1950cbc41565bc83af98
Giacomo Antonelli <GIACOMO.ANTONELLI@local.ittcesena.it>
2021-02-09 11:00:25 +0100

    Serializazzione/Deserializzazione Json

---
+++ TPSIT-PCTO-Server/JsonConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace TPSIT_PCTO_Server
+{
+    class JsonConverter
+    {
+        public void Converte(Part part)
+        {
+            string str = Newtonsoft.Json.JsonConvert.SerializeObject(part);
+            using (StreamWriter sw = new StreamWriter("output.json"))
+            {
+                sw.WriteLine(str);
+            }
+        }
+    }
+}
