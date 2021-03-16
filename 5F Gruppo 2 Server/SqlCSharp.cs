7c917e85d546cfde33a57a882fb9f2ea7724c46a
IGieckI <giacomo.antonelli3@gmail.com>
2021-03-16 11:29:16 +0100

    Terminale WPF, Update SqlCSharp, Input credenziali utente

--- 5F Gruppo 2 Server/SqlCSharp.cs
+++ 5F Gruppo 2 Server/SqlCSharp.cs
@@ -7,20 +7,30 @@ namespace _5F_Gruppo_2_Server
     class SqlCSharp
     {
         //Dario Bekic 5F Parte C# SQL PCTO
-        public SqlCSharp()
+        static void Main(string[] args)
         {
-
+            try
+            {
+                //Console.WriteLine(Operation("new|7|3|Tambu"));
+                //Console.WriteLine(Operation("new|534|5|Tambu"));
+                //Console.WriteLine(Operation("database"));
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("" + ex.Message);
+                Console.Read();
+            }
         }
-
-        public void Esegui(string ComandoDaClient)
+        static void ModificaSuPezzo(string comando, out SqlCommand command, SqlConnection cnn, SqlDataAdapter adapter)
         {
-            /*
-            * ADD|000|2
-            * ADD|000|10|Mercedes|ModelloX
-            * 
-            * WITHDRAW|000|2
-            */
-
+            command = new SqlCommand(comando, cnn);
+            adapter.InsertCommand = new SqlCommand(comando, cnn);
+            adapter.InsertCommand.ExecuteNonQuery();
+            command.Dispose();
+        }
+        static string Operation(string Comando)
+        {
             string connectionString;
             SqlConnection cnn;
             //connectionString = @"Data Source=PC1227;Initial Catalog=Magazzino;User ID=sa;Password=burbero2020";
@@ -30,73 +40,102 @@ namespace _5F_Gruppo_2_Server
             SqlCommand command;
             String sql;
             SqlDataAdapter adapter = new SqlDataAdapter();
+            string ComandoDaClient = Comando;
 
             string[] Elementi = ComandoDaClient.Split('|');
             cnn.Open();
 
-            if (Elementi[0] == "ADD" && Elementi.Length == 2)
-            {
-                ModificaSuPezzo("UPDATE dbo.Pezzo SET Quantità=Quantità+1 " + "WHERE Codice_Prodotto=" + Elementi[1], out command, cnn, adapter);
-                ModificaSuPezzo("INSERT INTO dbo.Movimento(QuantitàMossa, DataMovimento, IDProdottoCoinvolto, TipologiaMovimento) VALUES (1,'" + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff") + "'," + Elementi[1] + ",'" + Elementi[0] + "')", out command, cnn, adapter);
-            }
-            else if (Elementi[0] == "ADD" && Elementi.Length == 3)
-            {
-                ModificaSuPezzo("UPDATE dbo.Pezzo SET Quantità=Quantità+" + Elementi[2] + " WHERE Codice_Prodotto=" + Elementi[1], out command, cnn, adapter);
-                ModificaSuPezzo("INSERT INTO dbo.Movimento(QuantitàMossa, DataMovimento, IDProdottoCoinvolto, TipologiaMovimento) VALUES (" + Elementi[2] + ",'" + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff") + "'," + Elementi[1] + ",'" + Elementi[0] + "')", out command, cnn, adapter);
-            }
-            else if (Elementi[0] == "ADDNEW" && Elementi.Length == 5)
-            {
-                ModificaSuPezzo("INSERT INTO dbo.Pezzo(Codice_Prodotto, Quantità, Marca, Modello) VALUES(" + Elementi[1] + "," + Elementi[2] + "," + Elementi[3] + "," + Elementi[4] + ")", out command, cnn, adapter);
-                ModificaSuPezzo("INSERT INTO dbo.Movimento(QuantitàMossa, DataMovimento, IDProdottoCoinvolto, TipologiaMovimento) VALUES (" + Elementi[2] + ",'" + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff") + "'," + Elementi[1] + ",'" + Elementi[0] + "')", out command, cnn, adapter);
-            }
-            else if (Elementi[0] == "WITHDRAW")
-            {
-                sql = "SELECT Quantità FROM dbo.Pezzo WHERE Codice_Prodotto=" + Elementi[1];
-                command = new SqlCommand(sql, cnn);
-                int OutPutSelect = ((int)(command.ExecuteScalar())), QuantitàDaSottrarre = 1;
-                if (Elementi.Length == 3)
-                    QuantitàDaSottrarre = int.Parse(Elementi[2]);
+            if (IsThereError000(Elementi[0], Elementi.Length))
+                return "000|" + Elementi[1];
 
-                if (OutPutSelect - QuantitàDaSottrarre < 0)
-                    throw new InvalidExpressionException("...");
-                else
-                {
-                    ModificaSuPezzo("UPDATE dbo.Pezzo SET Quantità=Quantità-" + QuantitàDaSottrarre + " WHERE Codice_Prodotto=" + Elementi[1], out command, cnn, adapter);
-                    ModificaSuPezzo("INSERT INTO dbo.Movimento(QuantitàMossa, DataMovimento, IDProdottoCoinvolto, TipologiaMovimento) VALUES (" + Elementi[2] + ",'" + DateTime.Now.ToString("yyyy-MM-dd hh:mm:ss.fff") + "'," + Elementi[1] + ",'" + Elementi[0] + "')", out command, cnn, adapter);
-                }
-            }
-            else if (Elementi[0] == "SHOW")
+            if (Elementi[0] == "add")
             {
-                if (Elementi[1] == "dbo.Pezzo")
-                    sql = "SELECT * FROM dbo.Pezzo";
-                else
-                    sql = "SELECT * FROM dbo.Movimento";
+                sql = "SELECT Codice_Prodotto FROM dbo.Pezzo WHERE Codice_Prodotto=" + Elementi[1];
                 command = new SqlCommand(sql, cnn);
+
                 OutPutSelectAll = command.ExecuteReader();
-                if (Elementi[1] == "dbo.Pezzo")
-                {
-                    Console.WriteLine(OutPutSelectAll.GetName(0) + "    " + OutPutSelectAll.GetName(1) + "      " + OutPutSelectAll.GetName(2) + "                         " + OutPutSelectAll.GetName(3));
-                    while (OutPutSelectAll.Read())
-                        Console.WriteLine(OutPutSelectAll[0] + "                 " + OutPutSelectAll[1] + "             " + OutPutSelectAll[2] + "" + OutPutSelectAll[3]);
-                }
+                if (OutPutSelectAll.FieldCount == 1)
+                    ModificaSuPezzo("UPDATE dbo.Pezzo SET Quantità=Quantità+" + Elementi[2] + " WHERE Codice_Prodotto=" + Elementi[1], out command, cnn, adapter);
                 else
-                {
-                    Console.WriteLine(OutPutSelectAll.GetName(0) + "    " + OutPutSelectAll.GetName(1) + "      " + OutPutSelectAll.GetName(2) + "                 " + OutPutSelectAll.GetName(3) + "         " + OutPutSelectAll.GetName(4));
-                    while (OutPutSelectAll.Read())
-                        Console.WriteLine(OutPutSelectAll[0] + "              " + OutPutSelectAll[1] + "                  " + OutPutSelectAll[2] + "            " + OutPutSelectAll[3] + "                           " + OutPutSelectAll[4]);
+                    return "008|" + Elementi[1];
+            }
+            else if (Elementi[0] == "new")
+            {
+                sql = "SELECT Codice_Prodotto FROM dbo.Pezzo WHERE Codice_Prodotto=" + Elementi[1];
+                command = new SqlCommand(sql, cnn);
 
+                OutPutSelectAll = command.ExecuteReader();
+                while (OutPutSelectAll.Read())
+                {
+                    if (!OutPutSelectAll.IsDBNull(0))
+                        return "007|" + Elementi[1];
                 }
                 OutPutSelectAll.Close();
-            }
-            cnn.Close();
-        }
+                sql = "SELECT Nome_Prodotto FROM dbo.Pezzo WHERE Nome_Prodotto='" + Elementi[3] + "'";
 
-        static void ModificaSuPezzo(string comando, out SqlCommand command, SqlConnection cnn, SqlDataAdapter adapter)
+                command = new SqlCommand(sql, cnn);
+
+                OutPutSelectAll = command.ExecuteReader();
+
+                if (OutPutSelectAll.HasRows)
+                    return "006|" + Elementi[1];
+
+                ModificaSuPezzo("INSERT INTO dbo.Pezzo(Codice_Prodotto, Quantità, Nome_Prodotto) VALUES(" + Elementi[1] + "," + Elementi[2] + ",'" + Elementi[3] + "')", out command, cnn, adapter);
+            }
+            else if (Elementi[0] == "remove")
+            {
+                sql = "SELECT Quantità FROM dbo.Pezzo WHERE Codice_Prodotto=" + Elementi[1];
+
+                command = new SqlCommand(sql, cnn);
+
+                int OutPutSelect = ((int)(command.ExecuteScalar())), QuantitàDaSottrarre = 1;
+
+                QuantitàDaSottrarre = int.Parse(Elementi[2]);
+
+                if (OutPutSelect - QuantitàDaSottrarre < 0)
+                    return "009|" + Elementi[1];
+                else
+                    ModificaSuPezzo("UPDATE dbo.Pezzo SET Quantità=Quantità-" + QuantitàDaSottrarre + " WHERE Codice_Prodotto=" + Elementi[1], out command, cnn, adapter);
+            }
+            else if (Elementi[0] == "select")
+            {
+                sql = "SELECT * FROM dbo.Pezzo WHERE Codice_Prodotto=" + Elementi[1];
+                command = new SqlCommand(sql, cnn);
+                OutPutSelectAll = command.ExecuteReader();
+                while (OutPutSelectAll.Read())
+                    Console.WriteLine(OutPutSelectAll[0] + "|" + OutPutSelectAll[1] + "|" + OutPutSelectAll[2]);
+                OutPutSelectAll.Close();
+            }
+            else if (Elementi[0] == "database")
+            {
+                sql = "SELECT * FROM dbo.Pezzo";
+                command = new SqlCommand(sql, cnn);
+                OutPutSelectAll = command.ExecuteReader();
+                while (OutPutSelectAll.Read()) //Console.WriteLine(OutPutSelectAll.GetName(0) + "|" + OutPutSelectAll.GetName(1) + "|" + OutPutSelectAll.GetName(2));
+                    Console.WriteLine(OutPutSelectAll[0] + "|" + OutPutSelectAll[1] + "|" + OutPutSelectAll[2]);
+                OutPutSelectAll.Close();
+            }
+            else
+                return "001";
+
+            cnn.Close();
+            return "002";
+
+        }
+        static bool IsThereError000(string ActionCode, int ParameterNumber)
         {
-            command = new SqlCommand(comando, cnn);
-            adapter.InsertCommand = new SqlCommand(comando, cnn);
-            adapter.InsertCommand.ExecuteNonQuery();
-            command.Dispose();
+            if (ActionCode == "add" && ParameterNumber != 3)
+                return true;
+            else if (ActionCode == "new" && ParameterNumber != 4)
+                return true;
+            else if (ActionCode == "remove" && ParameterNumber != 3)
+                return true;
+            else if (ActionCode == "select" && ParameterNumber != 2)
+                return true;
+            else if (ActionCode == "database" && ParameterNumber != 1)
+                return true;
+            else
+                return false;
         }
     }
 }
